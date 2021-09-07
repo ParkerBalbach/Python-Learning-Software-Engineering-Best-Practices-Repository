28e394a92b0b2bfcad20b47dd719963cbf1859f1
Parker Balbach <parkerbalbach@u.boisestate.edu>
2021-09-06 18:41:04 -0600

    more beginner progs

---
+++ average-list/Programs/can-rid.py
@@ -0,0 +1,8 @@
+
+
+height = int(input("what is your height in in? "))
+
+if height >= 60:
+    print("You can ride")
+else:
+    print("nope")
---
+++ average-list/Programs/odd-even.py
@@ -0,0 +1,7 @@
+
+user_input = int(input("Please enter a number "))
+
+if user_input % 2 == 0:
+    print("your number is even")
+else:
+    print("your number is odd")
