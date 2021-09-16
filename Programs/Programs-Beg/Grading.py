79d920a387a561ef00854c1887f48c7a85239ade
Parker Balbach <parkerbalbach@u.boisestate.edu>
2021-09-15 19:15:48 -0600

    Grading

--- Programs/Programs-Beg/Grading.py
+++ Programs/Programs-Beg/Grading.py
@@ -1,2 +1,28 @@
 
 
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+    "Random": 50,
+}
+
+student_grades = {}
+
+for student in student_scores:
+    score = student_scores[student]
+    if score >= 90:
+        student_grades[student] = 'A'
+    elif score >= 80:
+        student_grades[student] = 'B'
+    elif score >= 70:
+        student_grades[student] = 'C'
+    elif score >= 60:
+        student_grades[student] = 'D'
+    elif score < 60:
+        student_grades[student] = 'F'
+
+
+print(student_grades)
