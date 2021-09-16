b8c6625253f38f1d3f784d29d432c38f3256ccf9
Parker Balbach <parkerbalbach@u.boisestate.edu>
2021-09-15 18:22:56 -0600

    caesar

---
+++ average-list/Programs/caesar-cipher.py
@@ -0,0 +1,35 @@
+
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+'s', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+'s', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(direction, text, shift):
+    end_text = ""
+    if direction == "decode":
+        shift *= -1
+    for char in text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"Here's the {direction}d result: {end_text}")
+
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+    caesar(direction, text, shift)
+
+    result = input("Type 'y' if you want to go again. Type 'n' to quit.\n")
+    if result == 'n':
+        print("Goodbye!")
+        should_continue = False
+
+
+
---
+++ average-list/Programs/user-input.py
@@ -0,0 +1,12 @@
+
+
+def greet(name,name2):
+    print(f"Hello {name}")
+    print(f"How do you do {name2}")
+
+
+name = input("Please enter your name: ")
+name2 = input("Please enter your mothers name: ")
+
+greet(name, name2)
+    
