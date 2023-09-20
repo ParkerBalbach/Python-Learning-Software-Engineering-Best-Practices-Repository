a82856bc81193228ff2188e8cf22ba13518f74ed
ParkerBalbach <70546594+ParkerBalbach@users.noreply.github.com>
2023-09-20 16:44:53 -0600

    Added comments for code clarity (#16)

--- Programs/Programs-Beg/user-input.py
+++ Programs/Programs-Beg/user-input.py
@@ -1,12 +1,23 @@
+"""Takes two arguments and prints a greeting.
 
+    Args:
+        name: name
+        name2: name2
 
-def greet(name,name2):
+    Returns: Printed Greetings for the two args
+"""
+def greet(name, name2):
+    # Print a greeting using the 'name' argument
     print(f"Hello {name}")
+    
+    # Print a follow-up greeting using the 'name2' argument
     print(f"How do you do {name2}")
 
-
+# Prompt the user to input their name
 name = input("Please enter your name: ")
-name2 = input("Please enter your mothers name: ")
 
+# Prompt the user to input their mother's name
+name2 = input("Please enter your mother's name: ")
+
+# Call the 'greet' function with the user's name and their mother's name as arguments
 greet(name, name2)
-    
