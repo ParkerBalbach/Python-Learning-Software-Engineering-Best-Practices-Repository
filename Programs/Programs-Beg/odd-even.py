716ddd26158fb92632f08cb1cb1bc7dca09a612e
ParkerBalbach <parkerbalbach@u.boisestate.edu>
2023-09-20 16:29:18 -0600

    Added comments for code clarity

--- Programs/Programs-Beg/odd-even.py
+++ Programs/Programs-Beg/odd-even.py
@@ -1,7 +1,10 @@
+# Prompt the user to input a number and convert it to an integer
+user_input = int(input("Please enter a number: "))
 
-user_input = int(input("Please enter a number "))
-
+# Check if the user's input is divisible by 2 (i.e., even)
 if user_input % 2 == 0:
-    print("your number is even")
+    # If the remainder of the division by 2 is 0, the number is even
+    print("Your number is even.")
 else:
-    print("your number is odd")
+    # If the remainder is not 0, the number is odd
+    print("Your number is odd.")
