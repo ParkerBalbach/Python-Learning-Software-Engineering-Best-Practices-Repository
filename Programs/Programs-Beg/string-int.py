99de05693254a4ee0e3707ce7606e023ad837f9a
ParkerBalbach <parkerbalbach@u.boisestate.edu>
2023-09-20 16:30:29 -0600

    Added comments for code clarity

--- Programs/Programs-Beg/string-int.py
+++ Programs/Programs-Beg/string-int.py
@@ -1,8 +1,14 @@
+# Prompt the user to input a two-digit number as a string
+user_input = input("Type a two-digit number: ")
 
-
-user_input = input("Type a two digit number: ")
-
+# Extract the first digit from the input string
 first_digit = user_input[0]
+
+# Extract the second digit from the input string
 second_digit = user_input[1]
 
-print(int(first_digit) + int(second_digit))
+# Convert the extracted first and second digits back to integers and then add them together
+result = int(first_digit) + int(second_digit)
+
+# Print the result of adding the two digits
+print(result)
