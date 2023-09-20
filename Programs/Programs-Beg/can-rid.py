f9f9536ae5ed518c7da9b14e29bbaf6d1a7df35b
ParkerBalbach <parkerbalbach@u.boisestate.edu>
2023-09-20 16:14:45 -0600

    Added comments for clarity

--- Programs/Programs-Beg/can-rid.py
+++ Programs/Programs-Beg/can-rid.py
@@ -1,8 +1,10 @@
+# Prompt the user to input their height in inches and convert it to an integer
+height = int(input("What is your height in inches? "))
 
-
-height = int(input("what is your height in in? "))
-
+# Check if the height is greater than or equal to 60 inches (5 feet)
 if height >= 60:
+    # If the height is 60 inches or more, print "You can ride"
     print("You can ride")
 else:
-    print("nope")
+    # If the height is less than 60 inches, print "nope"
+    print("Nope")
